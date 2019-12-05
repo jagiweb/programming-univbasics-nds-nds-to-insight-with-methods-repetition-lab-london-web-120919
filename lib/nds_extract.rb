b90a4a125cd1961e24e9c308e0977773915c9a33
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-05 11:01:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,12 +35,13 @@ def list_of_directors(source)
 end
 
 def total_gross(source)
-  total = 0
-  i = 0
-  while i < directors_totals(source).length do
-    total += gross_for_director(source)[list_of_directors[i]]
-  end
-  total
+  # total = 0
+  # i = 0
+  # while i < directors_totals(source).length do
+  #   total += gross_for_director(source)[list_of_directors[i]]
+  # end
+  # total
+  gross_for_director(source)[list_of_directors[i]]
 end
 
 # Write this implementation
